6609463d93fd908b9c7a6c0617f45d5e19519e4f
andreanicte02 <andreanicte02@gmail.com>
2024-07-11 21:44:14 -0600

    f: selector

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {PublicRoute} from "./routes/PublicRoute.tsx";
 
-ReactDOM.createRoot(document.getElementById('#root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PublicRoute />
   </React.StrictMode>,
